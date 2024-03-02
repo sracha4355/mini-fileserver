c1d2c54de4876f23e2406b4da3a57e2b714c7936
Satvik Racharla <satvikracharla@pop-os.localdomain>
2024-03-02 17:17:24 -0500

    deleted old code for command parsing

--- CommandParser/tests/misc/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <parser.h>
-#include <tokenizer.h>
-int main(){
-	parser::tokenizer tokenizer("get");
-	tokenizer.tokenize_input_stream();
-	parser::parser parser(tokenizer.get_token_stream());
-	return 0;
-}
